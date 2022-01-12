7b504625191855512c3721c40bdb7a442a6e2fd3
Adal Michael García <aodarug@gmail.com>
2022-01-12 15:45:01 -0500

    Separacion de codigo de configuracion fragmento Keybinding

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -30,6 +30,7 @@ from libqtile import bar, layout, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
+from settings.keys import mod, keys
 
 from os import path
 import subprocess
@@ -73,74 +74,6 @@ def icon(fg='#2d2a2e', bg='#2d2a2e', fontsize=16, text="?"):
         padding=3
     )
 
-mod = "mod4"
-terminal = "alacritty"
-
-keys = [
-    # Switch between windows
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split(), desc="Toggle between split and unsplit sides of stack"),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-
-    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-    
-    # Menu
-    Key([mod], "m", lazy.spawn("rofi -show drun")),
-    Key([mod], "s", lazy.spawn("scrot")),
-    Key([mod], "e", lazy.spawn("thunar")),
-    Key([mod], "b", lazy.spawn("google-chrome-stable")),
-    Key([mod, "shift"], "s", lazy.spawn("flameshot gui -p /home/adal/Pictures/Screenshots/")),
-    Key([], "XF86Calculator", lazy.spawn("kcalc")),
-
-    # Window Nav
-    Key([mod, "shift"], "m", lazy.spawn("rofi -show")),
-
-    # Volume
-    Key([], "XF86AudioLowerVolume", lazy.spawn("pamixer --decrease 5")),
-    Key([], "XF86AudioRaiseVolume", lazy.spawn("pamixer --increase 5")),
-    Key([], "XF86AudioMute", lazy.spawn("pamixer --toggle-mute")),
-
-    # Music Control
-    Key([], "XF86AudioPlay", lazy.spawn("playerctl play-pause")),
-    Key([], "XF86AudioStop", lazy.spawn("playerctl stop")),
-    Key([], "XF86AudioNext", lazy.spawn("playerctl next")),
-    Key([], "XF86AudioPrev", lazy.spawn("playerctl previousb")),
-
-    # Brightness
-    Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +10%")),
-    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10%-")),
-    
-]
 
 groups = [Group(i) for i in [
     " ", " ", " "," "," "," "
@@ -309,9 +242,6 @@ screens = [
                     background=["#2d2a2e", "#2d2a2e"],
                     fontsize=14,
                     font='UbuntuMono Nerd Font Bold',
-                ),
-                widget.Image(
-                    filename=path.join(path.expanduser('~'), '.config', 'qtile', 'img', 'bar1.png')
                 ),
                 widget.CurrentLayoutIcon(
                     foreground=["#2d2a2e", "#2d2a2e"],
